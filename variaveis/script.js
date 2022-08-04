d1c906324fb2272175941c14ed09e75ade5dcdb7
Victor Roberto <robertovictor0702@gmail.com>
2022-08-03 22:40:11 -0300

    variables

---
+++ variaveis/script.js
@@ -0,0 +1,47 @@
+var nome = 'Victor';
+var idade = 21;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = preco * totalComprado;
+
+console.log(totalPreco);
+
+var sobrenome = 'Roberto',
+    cidade = 'SBC',
+    estado = 'SP';
+
+console.log(sobrenome, cidade, estado)
+
+var semDefinir; 
+
+console.log(semDefinir);
+
+//exemplo de hosting abaixo 
+console.log(comida);
+var comida = 'Pizza';
+
+
+var time = 'Corinthians';
+var time = 'Coringão';
+
+console.log(time);
+
+// Exercicio
+
+var nome = 'Bruno';
+var idade = 22;
+var comidaFavorita;
+comidaFavorita = 'strogonoff'
+var var1,
+    var2,
+    var3,
+    var4,
+    var5;
+
+console.log(nome, idade, comidaFavorita)
+console.log(var1, var2, var3, var4, var5)
+
