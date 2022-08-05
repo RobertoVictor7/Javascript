3bf1442286d1f69b457dc55480fd4d1510322100
Victor Roberto <robertovictor0702@gmail.com>
2022-08-05 00:03:11 -0300

    numeros-e-operadores

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,27 @@
+// numerais
+
+var idade = 23;
+var exponencial = 2e10;
+var decimal = 3.14;
+
+console.log(idade, exponencial, decimal);
+
+// operadores aritiméticos basicos
+
+var soma = 100 + 50;
+var subtracao = 100 - 50;
+var multiplicacao = 100 * 2;
+var divisao = 100 / 2;
+var expoente = 2 ** 4; 
+var modulo = 14 % 5;
+
+console.log(soma, subtracao, multiplicacao, divisao, expoente, modulo)
+
+// operadores aritiméticos (Strings)
+
+var soma = '100' + 50;
+var subtracao = '100' - 50;
+var multiplicacao = '100' * '2';
+var divisao = 'Comprei 10' / 2;
+
+console.log(soma, subtracao, multiplicacao, divisao)
