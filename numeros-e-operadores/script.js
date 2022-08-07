8d636f5972def60f75e7c0cd0383501752698dfd
Victor Roberto <robertovictor0702@gmail.com>
2022-08-07 03:19:35 -0300

    operadores aritiméticos unários

--- numeros-e-operadores/script.js
+++ numeros-e-operadores/script.js
@@ -25,3 +25,52 @@ var multiplicacao = '100' * '2';
 var divisao = 'Comprei 10' / 2;
 
 console.log(soma, subtracao, multiplicacao, divisao)
+
+// a ordem importa, comepça por multiplicacao e divisao, depois soma e subtracao.
+// utilizar parênteses para priorizar uma equação.
+
+// Operadores aritiméticos unários;
+
+var incremento = 5;
+console.log(incremento++);
+console.log(incremento);
+
+var incremento2 = 5;
+console.log(++incremento2);
+console.log(incremento2);
+
+var decremento = 5;
+console.log(--decremento);
+
+var idade = '23';
+var somaIdade = 5;
+console.log(+idade + somaIdade);
+
+// exercicio
+
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+
+var nan1 = 3 / '2s';
+console.log(nan1)
+
+var nan2 = 5 * nan1;
+console.log(nan2)
+
+var n1 = '200';
+var n2 = 50;
+var soma = +n1 + n2;
+console.log(soma)
+
+var base = 5;
+base++;
+console.log(base);
+
+// como dividir o peso por 2?
+
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade;
+
+
+console.log(peso)
