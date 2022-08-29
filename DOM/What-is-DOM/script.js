e9044f30897bb83c1956fc16bd17ea9a0d8e133e
Victor Roberto <robertovictor0702@gmail.com>
2022-08-29 14:59:55 -0300

    DOM

---
+++ DOM/What-is-DOM/script.js
@@ -0,0 +1,15 @@
+// Retorne o url da página atual utilizando o objeto window
+
+console.log(`O url atual da pagina é ${window.location.href}`)
+
+// Selecione o primeiro elemento da pagina que possua a classe ativo
+
+console.log(document.querySelector('.ativo'))
+
+// retorne a linguagem do navegador
+
+console.log(navigator.language)
+
+//retorne a largura da janela
+
+console.log(window.innerWidth)
