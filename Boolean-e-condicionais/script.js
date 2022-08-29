ec6d9db5ae87887f727d095771d0daab79592a54
Victor Roberto <robertovictor0702@gmail.com>
2022-08-29 17:37:46 -0300

    nova pasta

---
+++ Boolean-e-condicionais/script.js
@@ -0,0 +1,10 @@
+var possuiGraduacao = true;
+var possuiDoutorado = true;
+
+if(possuiGraduacao) {
+    console.log('é verdadeiro')
+} else if(possuiDoutorado) { 
+    console.log('é falso')
+} else {
+    console.log('Não possui nada')
+}
