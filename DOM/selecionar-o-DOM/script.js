1af6cfa53ba9db19af167131f5323fd17d8997d5
Victor Roberto <Robertovictor0702@gmail.com>
2022-09-01 21:38:39 -0300

    Selecionand o DOM

---
+++ DOM/selecionar-o-DOM/script.js
@@ -0,0 +1,13 @@
+const animais = document.getElementById('animais');
+
+const gridSection = document.getElementsByClassName('grid-section')
+console.log(gridSection)
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi)
+
+const linksInternos = document.querySelector('[href^="#"]')
+console.log(linksInternos)
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg)
