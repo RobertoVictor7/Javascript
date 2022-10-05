0ce3caccef04bf0929bd2ddcab9180c45b3e4af8
Victor Roberto <robertovictor0702@gmail.com>
2022-10-05 14:07:03 -0300

    native-host-user

---
+++ Objetos/native-host-user/script.js
@@ -0,0 +1,22 @@
+// Liste 5 objetos nativos
+Object
+String
+Array
+Function
+Number
+
+// liste 5 objetos do browser
+Window
+Document
+History
+HTMLCollection
+NodeList
+
+//liste 2 metodos, propriedades ou Objetos
+//presdentes no chrome que nao existe no firefox
+
+if(typeof document.webkithidden !== "undefined") {
+    console.log('Existe');
+} else {
+    console.log('Não Existe');
+}
