5bfdc357484c3cbb53c3a4912156e76219ee2eda
Victor Roberto <Robertovictor0702@gmail.com>
2022-10-19 16:52:17 -0300

    modal

---
+++ Efeitos-no-Dom/modal/js/script.js
@@ -0,0 +1,9 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import accordion from './modules/accordion.js';
+import tabNav from './modules/tab-nav.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+accordion();
+tabNav();
