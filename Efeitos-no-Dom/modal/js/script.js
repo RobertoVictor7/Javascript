edb6521ff9e2f2a0ffa7ceb08c1f2193e03a1963
Victor Roberto <robertovictor0702@gmail.com>
2022-10-23 01:19:39 -0300

    modal completo

--- Efeitos-no-Dom/modal/js/script.js
+++ Efeitos-no-Dom/modal/js/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import accordion from './modules/accordion.js';
 import tabNav from './modules/tab-nav.js';
+import initModal from './modules/modal.js'
 
 initScrollSuave();
 initAnimacaoScroll();
 accordion();
 tabNav();
+initModal();
