af8de6bb037ade11e1214f5033f0e8765e2c4161
Victor Roberto <robertovictor0702@gmail.com>
2022-11-08 14:24:54 -0300

    Data Object

---
+++ Efeitos-no-Dom/data-object/js/script.js
@@ -0,0 +1,21 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import accordion from './modules/accordion.js';
+import tabNav from './modules/tab-nav.js';
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initAnimaNumeros from './modules/anima-numeros.js';
+import initFuncionamento from './modules/funcionamento.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+accordion();
+tabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
