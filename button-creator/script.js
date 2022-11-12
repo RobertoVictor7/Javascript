05eb7fa59b7c6a955ca506c9b2d74ed49f49fc7f
Victor <robertovictor0702@gmail.com>
2022-11-12 17:36:31 -0300

    button creator 2

---
+++ button-creator/script.js
@@ -0,0 +1,50 @@
+const controles = document.querySelector('#controles');
+const cssText = document.querySelector('.css');
+const btn = document.querySelector('.btn');
+
+const handleStyle = {
+    element: btn,
+    texto(value){
+        this.element.innerText = value;
+    },
+    color(value){
+        this.element.style.color = value;
+    },
+    backgroundColor(value) {
+        this.element.style.backgroundColor = value;
+    },
+    height(value) {
+        this.element.style.height = value + 'px';
+    },
+    width(value){
+        this.element.style.width = value + 'px';
+    },
+    border(value) {
+        this.element.style.border = value + 'px solid'
+    },
+    borderRadius(value) {
+        this.element.style.borderRadius = value + 'px';
+    },
+    fontFamily(value) {
+        this.element.style.fontFamily = value;
+    },
+    fontSize(value){
+        this.element.style.fontSize = value + 'px';
+    }
+}
+
+function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+   
+    handleStyle[name](value);
+}
+
+function showCss(){
+    cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join('; </span><span>');
+    console.log(btn.style)
+}
+
+btn.addEventListener('click', showCss);
+
+controles.addEventListener('change', handleChange)
