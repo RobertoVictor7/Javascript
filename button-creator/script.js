7e4b99ca4d2f6542f071bc41772b4f058375a408
Victor <robertovictor0702@gmail.com>
2022-11-14 01:12:13 -0300

    localStorage adicionado

--- button-creator/script.js
+++ button-creator/script.js
@@ -1,6 +1,7 @@
 const controles = document.querySelector('#controles');
 const cssText = document.querySelector('.css');
 const btn = document.querySelector('.btn');
+const clearBtn = document.querySelector('.clearData');
 
 const handleStyle = {
     element: btn,
@@ -35,7 +36,7 @@ const handleStyle = {
         this.element.style.textTransform = value;
     },
     fontSize(value){
-        this.element.style.fontSize = value + 'px';
+        this.element.style.fontSize = value + 'rem';
     }
 }
 
@@ -44,13 +45,34 @@ function handleChange(e) {
     const value = e.target.value;
    
     handleStyle[name](value);
+    saveValues(name, value);
+    showCss();
 }
 
 function showCss(){
     cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join('; </span><span>');
-    console.log(btn.style)
 }
 
 btn.addEventListener('click', showCss);
+controles.addEventListener('change', handleChange);
 
-controles.addEventListener('change', handleChange)
+function saveValues(name, value){
+    localStorage[name] = value;
+}
+
+function setValues(){
+    const properties = Object.keys(localStorage);
+    properties.forEach(propertie => {
+        handleStyle[propertie](localStorage[propertie]);
+        controles.elements[propertie].value = localStorage[propertie];
+    })
+    showCss();
+}
+
+function clearData() {
+    localStorage.clear();
+}
+
+clearBtn.addEventListener('click', clearData);
+
+setValues();
