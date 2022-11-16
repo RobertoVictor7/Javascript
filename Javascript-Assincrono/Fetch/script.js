b5168052c0f55afed774ff2fae0687edde48065e
Victor <robertovictor0702@gmail.com>
2022-11-16 13:44:12 -0300

    fetch image

--- Javascript-Assincrono/Fetch/script.js
+++ Javascript-Assincrono/Fetch/script.js
@@ -1,13 +1,30 @@
-const sobre = fetch("./sobre.html");
+const cep = fetch('https://viacep.com.br/ws/09725051/json/')
 
-const div = document.createElement('div');
+cep.then(data => {
+    return data.json();
+}).then((body) => {
+    console.log(body);
+    const conteudo = document.querySelector('.conteudo');
+    
+    conteudo.innerText = body.logradouro;
+})
 
-sobre
-  .then((data) => {
-    return data.text();
-  })
-  .then((body) => {
-    div.innerHTML = body;
-    const titulo = div.querySelector('h1');
-    console.log(titulo)
+// imagem
+ 
+const imagem = fetch("./teste.png");
+imagem
+  .then(data => data.blob())
+  .then(body => {
+    const blobUrl = URL.createObjectURL(body)
+    const imagemDom = document.querySelector('img')
+    imagemDom.src = blobUrl;
   });
+
+// clone
+
+const cep2 = fetch('https://viacep.com.br/ws/09725051/json/')
+
+cep2.then(data => data.clone())
+.then(body => {
+    console.log(body);
+})
