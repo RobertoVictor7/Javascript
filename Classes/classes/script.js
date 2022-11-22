3f2a58c9b1b94841f27dddc7ff96ed65e47a6781
Victor <robertovictor0702@gmail.com>
2022-11-22 00:00:33 -0300

    classes

---
+++ Classes/classes/script.js
@@ -0,0 +1,58 @@
+// function Button(text, background) {
+//   this.text = text;
+//   this.background = background;
+// }
+
+// Button.prototype.element = function () {
+//   const buttonElement = document.createElement("button");
+//   buttonElement.innerText = this.text;
+//   buttonElement.style.backgroundColor = this.background;
+//   return buttonElement;
+// };
+
+class Button {
+  constructor(text, background, color) {
+    this.text = text;
+    this.background = background;
+    this.color = color;
+  }
+  element() {
+    const buttonElement = document.createElement("button");
+    buttonElement.innerText = this.text;
+    buttonElement.style.backgroundColor = this.background;
+    buttonElement.style.color = this.color;
+    return buttonElement;
+  }
+  appendTo(target) {
+    const targetElement = document.querySelector(target);
+    targetElement.appendChild(this.element());
+    return targetElement;
+  }
+  static blueButton(text) {
+    return new Button(text,'blue', 'white')
+  }
+}
+
+const botaoAzul = Button.blueButton('comprar');
+
+// console.log(blueButton.appendTo('body'));
+
+// class Button {
+//     constructor(options) {
+//         this.options = options;
+//     }
+//     static createButton(text, background) {
+//         const buttonElement = document.createElement("button");
+//         buttonElement.innerText = text;
+//         buttonElement.style.backgroundColor = background;
+//         return buttonElement;
+//     }
+// }
+
+// const blueButton = new Button({
+//     backgroundColor: 'blue',
+//     text: 'Comprar',
+//     color: 'white'
+// })
+
+// const blueButtonStatic = Button.createButton('clique', 'blue');
